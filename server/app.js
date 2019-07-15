4594abadb6a67902c08d44578d8063b3aeda4658
skd1993 <shobhit.kr@outlook.com>
2019-07-15 17:02:21 +0530

    apollo install

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 const app = express();
+app.use("cors");
 mongoose.connect('mongodb://johnwick76:test123@ds145752.mlab.com:45752/graphql', { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log('connected to mLab');
